af59a301721ed26f3f073c812d41e30a6e858533
Pablo Duboue <pablo.duboue@gmail.com>
2012-11-04 07:54:07 -0500

    Debugged driver

--- hg/ntcir.py
+++ hg/ntcir.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from shutil import copyfile
 
@@ -10,10 +11,10 @@ def load_queries(query_file):
     queries = []
 
     for line in file(query_file):
-        parts = line.split('\t')
-        if parts.length == 3:
+        parts = line.strip().split('\t')
+        if len(parts) == 3:
             queries.append({'id' : parts[0], 'query': parts[2] })
-        elif parts.length == 2:
+        elif len(parts) == 2:
             queries.append({'id' : parts[0], 'query': parts[1] })
         else:
             sys.exit("Bad number of tabs in query file, line '%s'" % (line,))
@@ -27,8 +28,8 @@ def register_query_pages(query_id, query_str, cursor, ntcir_urls_folder, ntcir_h
     rank = 0
 
     for file_name in file("%s/%s.MAND.tsv" % (ntcir_urls_folder, query_id,)):
-        full_name = "%s/%s" % (ntcir_htmls_folder, file_name)
-        url = "http://%s" % (full_name,)
+        full_name = "%s/%s" % (ntcir_htmls_folder, file_name.strip())
+        url = "file://%s" % (full_name,)
         rid = web_search.copy_existing_page(cursor, cache_files_folder, full_name, url)
         web_search.ensure_page_query_link(cursor, cache_files_folder, qid, rank, url)
         rank += 1
@@ -39,7 +40,7 @@ if __name__ == '__main__':
     #
     ini_file = sys.argv[1]
     query_file = sys.argv[2]
-    run_number = sys.argv[3]
+    run_number = int(sys.argv[3])
 
     ini = load_ini(ini_file)
     queries = load_queries(query_file)
@@ -52,7 +53,9 @@ if __name__ == '__main__':
     ntcir_urls_folder = ini['ntcir_urls_folder']
     ntcir_htmls_folder = ini['ntcir_htmls_folder']
 
-    cache_folder = ini.get('cache_folder', ini.get('tmp_folder', './tmp') + "/cache")
+    base_tmp = ini.get('tmp_folder', './tmp')
+
+    cache_folder = ini.get('cache_folder',  "%s/cache" % (base_tmp,))
     (conn, cursor) = web_search.open_db(cache_folder)
 
     output_file_desktop = "%s-E-D-MAND-%d.tsv" % (team_name, run_number)
@@ -67,10 +70,15 @@ if __name__ == '__main__':
         query_id = query['id']
         query_str = query['query']
 
+        tmp = "%s/%s" % (base_tmp, query_id)
+        os.mkdir(tmp)
+        ini['tmp_folder'] = tmp
+
         if web_search.find_query(cursor, query_str, 'NTCIR') is None:
             register_query_pages(query_id, query_str, cursor, ntcir_urls_folder, ntcir_htmls_folder, cache_folder)
+            conn.commit()
 
-        results = one_click_search(ini, query_str, (1000, 'DESKTOP'), (280, 'MOBILE'))
+        results = one_click_search(ini, query_str, [(1000, 'DESKTOP'), (280, 'MOBILE')])
         for (output, result) in [(output_desktop, results['DESKTOP']), (output_mobile, results['MOBILE'])]:
             output.write('%s\tOUT\t%s\n' % (query_id, re.sub('\n', ' ', result[0])))
             for evidence in result[1]:
